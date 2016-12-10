1d013a8ea3fd47d0d6801fdab6c7c3c246c933b2
b03902042 <b03902042@oasis1.csie.ntu.edu.tw>
2016-12-11 02:46:41 +0800

    congestion control on receiver

--- hw2/receiver.c
+++ hw2/receiver.c
@@ -12,14 +12,28 @@
 #include "utility.h"
 #include "packet.h"
 
+#define BUFFER_SIZE 32
+
+typedef struct buffer {
+	char data[BUF_LEN];
+	int len;
+	int used;
+} Buffer;
+
 int port;
 char file_path[BUF_LEN];
+
+Buffer buffer[BUFFER_SIZE];
+int total_used = 0;
+int base = 1, seq = 1;
 
 int main(int argc, char* argv[])
 {
 	int v;
 	if ((v = find_arg(argc, argv, "-port")) != -1) port = atoi(argv[v+1]);
 	if ((v = find_arg(argc, argv, "-file")) != -1) strcpy(file_path, argv[v+1]);
+
+	memset(buffer, 0, BUFFER_SIZE);
 
 	int socket_fd;
 	struct sockaddr_in my_addr;
@@ -36,12 +50,14 @@ int main(int argc, char* argv[])
 		die("bind failed");
 	}
 
+	/* open destination file */
 	int fd;
 	if ((fd = open(file_path, O_WRONLY | O_CREAT, 0644)) < 0) die("open file failed");
 	int rcv_len;
+
 	struct sockaddr_in addr;
 	int addr_len = sizeof(addr);
-	Packet rcv_pkt, sd_pkt;
+	Packet rcv_pkt, snd_pkt;
 	int type;
 	while (1) {
 		if ((rcv_len = recvfrom(socket_fd, &rcv_pkt, sizeof(Packet), 0, (struct sockaddr*)&addr, &addr_len)) < 0) {
@@ -49,40 +65,63 @@ int main(int argc, char* argv[])
 		}
 		
 		type = rcv_pkt.h.type;
+		seq = rcv_pkt.h.seq;
 		if (type == DATA) {
-			printf("recv\tdata\t#%d\n", rcv_pkt.h.seq);
-			if (write(fd, rcv_pkt.data, rcv_pkt.h.len) < 0) die("write to file failed");
-			sd_pkt.h.type = ACK;		
+			if (seq-base >= BUFFER_SIZE) {
+				/* out of range */
+				printf("drop\tdata\t#%d\n", seq);
+				if (total_used == BUFFER_SIZE) {
+					printf("flush\n");
+					for (int i = 0; i < BUFFER_SIZE; i++)
+						if (write(fd, buffer[i].data, buffer[i].len) < 0) die("write to file failed");
+					base += BUFFER_SIZE;
+					memset(buffer, 0, BUFFER_SIZE);
+					total_used = 0;
+				}
+				continue;
+			}
+			else if (buffer[seq-base].used == 1) {
+				/* already received */
+				printf("ignr\tdata\t#%d\n", seq);
+				snd_pkt.h.type = ACK;
+			}
+			else {
+				/* ack it */
+				printf("recv\tdata\t#%d\n", seq);
+				memcpy(buffer[seq-base].data, rcv_pkt.data, rcv_pkt.h.len);
+				buffer[seq-base].len = rcv_pkt.h.len;
+				buffer[seq-base].used = 1;
+				snd_pkt.h.type = ACK;
+				total_used += 1;
+			}
 		}
 		else if (type == FIN) {
-			sd_pkt.h.type = FINACK;
-		}
-		else {
-			fprintf(stderr, "receive strange type: %s\n", TYPE[sd_pkt.h.type]);
-			exit(1);
+			printf("recv\tfin\n");
+			snd_pkt.h.type = FINACK;
 		}
 
-		memcpy(&sd_pkt.h.src, &rcv_pkt.h.dest, sizeof(struct sockaddr_in));
-		memcpy(&sd_pkt.h.dest, &rcv_pkt.h.src, sizeof(struct sockaddr_in));
-		sd_pkt.h.len = 0;
-		sd_pkt.h.seq = rcv_pkt.h.seq;
-		memset(sd_pkt.data, 0, BUF_LEN);
-		if (sendto(socket_fd, &sd_pkt, sizeof(Packet), 0, (struct sockaddr*)&addr, addr_len) < 0) {
+		memcpy(&snd_pkt.h.src, &rcv_pkt.h.dest, sizeof(struct sockaddr_in));
+		memcpy(&snd_pkt.h.dest, &rcv_pkt.h.src, sizeof(struct sockaddr_in));
+		snd_pkt.h.len = 0;
+		snd_pkt.h.seq = seq;
+		memset(snd_pkt.data, 0, BUF_LEN);
+		if (sendto(socket_fd, &snd_pkt, sizeof(Packet), 0, (struct sockaddr*)&addr, addr_len) < 0) {
 			die("sendto failed");
 		}
-		if (sd_pkt.h.type == ACK) {
-			printf("send\tack\t#%d\n", sd_pkt.h.seq);
+		if (snd_pkt.h.type == ACK) {
+			printf("send\tack\t#%d\n", snd_pkt.h.seq);
 		}
-		else if (sd_pkt.h.type == FINACK) {
+		else if (snd_pkt.h.type == FINACK) {
 			printf("send\tfinack\n");
 			break;
 		}
-		else {
-			fprintf(stderr, "send strange type: %s\n", TYPE[sd_pkt.h.type]);
-			exit(1);
-		}
 	}
 
+	for (int i = 0; buffer[i].used == 1 && i < BUFFER_SIZE; i++) {
+		if (i == 0) printf("flush\n");
+		if (write(fd, buffer[i].data, buffer[i].len) < 0) die("write to file failed");
+	}
+	
 	close(socket_fd);
 
 	return 0;
